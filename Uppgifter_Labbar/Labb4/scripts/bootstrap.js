3981d3f36d3cb97e57d623e47f09753649c34fbf
7acc <jacc321@gmail.com>
2016-05-31 13:33:49 +0200

    more stuff has happened:9

--- Uppgifter_Labbar/Labb4/scripts/bootstrap.js
+++ Uppgifter_Labbar/Labb4/scripts/bootstrap.js
@@ -2359,5 +2359,12 @@ if (typeof jQuery === 'undefined') {
       Plugin.call($spy, data)
     })
   })
-
+  document.onscroll = function () {
+      if ($(window).scrollTop() > $('header').height()) {
+          $('nav > div.navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top');
+      }
+      else {
+          $('nav > div.navbar').removeClass('navbar-fixed-top').addClass('navbar-static-top');
+      }
+  };
 }(jQuery);
