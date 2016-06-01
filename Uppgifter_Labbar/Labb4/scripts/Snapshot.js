1b2279c7bb47cfbf831635034f71921bf1df4020
7acc <jacc321@gmail.com>
2016-06-01 14:52:09 +0200

    kinda made the video page

---
+++ Uppgifter_Labbar/Labb4/scripts/Snapshot.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    alert('hej')
+    $('snapshot1').on('click', snap1);
+    $('snapshot2').on('click', snap2);
+});
+
+function snap1() {
+    var canvas = document.getElementById('mycanvas1')
+    var iframe = document.getElementById('myvideo1')
+    var video = (iframe.contentDocument)
+    canvas.getContext('2d').drawImage(video, 0, 0, 400, 320)
+}
+function snap2() {
+    var canvas = document.getElementById('mycanvas2')
+    var video = document.getElementById('myvideo2')
+    canvas.getContext('2d').drawImage(video, 0, 0, 350, 300)
+}
