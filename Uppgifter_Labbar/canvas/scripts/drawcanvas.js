50b8247015e1670b533e738aaa8a64d3bc98fe6e
7acc <jacc321@gmail.com>
2016-06-01 13:12:19 +0200

    git commit -a -m"HEELOOO"

---
+++ Uppgifter_Labbar/canvas/scripts/drawcanvas.js
@@ -0,0 +1,21 @@
+﻿
+// A $( document ).ready() block.
+$(document).ready(function () {
+    console.log("ready!");
+
+    draw();
+});
+
+function draw() {
+    //javascript
+    var canvas = document.getElementById('mycanvas');
+    var ctx = canvas.getContext('2d');
+    ctx.fillRect(65, 10, 10, 100);
+    ctx.fillRect(100, 10, 10, 100);
+    ctx.fillRect(60,55 , 60, 5);
+    // Jquery
+    var jQuerCanvas = $('mycanvas').getContext('2d').fillRect(10, 10, 280, 80);
+}
+function drawSomething() {
+    var canvas = Document.getElementById('mycanvas')
+}
---
+++ Uppgifter_Labbar/snapshot/scripts/JavaScript.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('#snapshot').on('click', snap);
+});
+
+function snap() {
+    var canvas = document.getElementById('mycanvas')
+    var video = document.getElementById('myvideo')
+    canvas.getContext('2d').drawImage(video, 0,0,400,320)
+}
