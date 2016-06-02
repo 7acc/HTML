d0c3f6f2e1808e430a5de83bd2d4f7658a75aaf3
7acc <jacc321@gmail.com>
2016-06-02 14:25:14 +0200

    okey. i think im done

---
+++ Uppgifter_Labbar/Labb4/scripts/rating.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    console.log("ready!");
+
+
+ 
+
+    console.log("hmmm");
+    $(".radio input[type='radio']").on('click', function () {
+        if ($("#awesome").is(":checked")) {
+            var content = document.getElementById("good").innerHTML;
+            
+            $("#smiley").html(content);
+          
+        } else if ($("#meh").is(":checked")) {
+            var content = document.getElementById("meeh").innerHTML;
+            
+            $("#smiley").html(content);
+            
+        }
+        else if ($("#terrible").is(":checked")) {
+            var content = document.getElementById("bad").innerHTML;
+           
+            $("#smiley").html(content);
+           
+        }
+    })
+});
